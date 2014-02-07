38d9bdd7b9c92cd0a4156a23da5be62dd35e5493
Won-Hyung Park <whyung.park@samsung.com>
2014-02-07 16:10:36 +0900

    first commit

---
+++ src/com/example/superrookie/CallDetectService.java
@@ -0,0 +1,34 @@
+package com.example.superrookie;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class CallDetectService extends Service {
+	private CallHelper callHelper;
+	@Override
+	public IBinder onBind(Intent arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@Override
+	public int onStartCommand(Intent intent, int flags, int startId) {
+		// TODO Auto-generated method stub
+		callHelper = new CallHelper(this);
+		
+		int result = super.onStartCommand(intent, flags, startId);
+		callHelper.start();
+		
+		return result;
+	}
+	
+	@Override
+	public void onDestroy() {
+		// TODO Auto-generated method stub
+		super.onDestroy();
+		
+		callHelper.stop();
+	}	
+}
+
---
+++ src/com/example/superrookie/CallHelper.java
@@ -0,0 +1,57 @@
+package com.example.superrookie;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.telephony.PhoneStateListener;
+import android.telephony.TelephonyManager;
+import android.widget.Toast;
+
+public class CallHelper {
+	private Context context;
+	private TelephonyManager telephonyManager;
+	private CallStateListener callStateListener;
+	private OutgoingReceiver outgoingReceiver;
+	
+	private class CallStateListener extends PhoneStateListener {				
+		@Override
+		public void onCallStateChanged(int state, String incomingNumber) {
+			// TODO Auto-generated method stub
+			switch (state) {
+			case TelephonyManager.CALL_STATE_RINGING:
+				Toast.makeText(context, "Incoming : " + incomingNumber, Toast.LENGTH_LONG).show();
+				
+			}
+		}
+	}
+	
+	private class OutgoingReceiver extends BroadcastReceiver {
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			// TODO Auto-generated method stub
+			String number = intent.getStringExtra(Intent.EXTRA_PHONE_NUMBER);
+			Toast.makeText(context, "Outgoing: " + number, Toast.LENGTH_LONG).show();
+		}
+	}
+	
+	public CallHelper(Context context) {
+		this.context = context;
+		
+		callStateListener = new CallStateListener();
+		outgoingReceiver = new OutgoingReceiver();
+	}
+	
+	public void start() {
+		telephonyManager = (TelephonyManager)context.getSystemService(Context.TELEPHONY_SERVICE);
+		telephonyManager.listen(callStateListener, PhoneStateListener.LISTEN_CALL_STATE);
+		
+		IntentFilter intentFilter = new IntentFilter(Intent.ACTION_NEW_OUTGOING_CALL);
+		context.registerReceiver(outgoingReceiver, intentFilter);
+	}
+	
+	public void stop() {
+		telephonyManager.listen(callStateListener, PhoneStateListener.LISTEN_NONE);
+		context.unregisterReceiver(outgoingReceiver);
+	}
+}
